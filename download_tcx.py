0c80fccbd42780fb10f22ed6ffa57da8fc45b167
ken-miyashita <Ken.Miyashita@sony.com>
2017-01-21 23:18:18 +0900

    run both tests (enumerate log / get TCX)

--- download_tcx.py
+++ download_tcx.py
@@ -102,8 +102,9 @@ server.browser_authorize()
 # Authorization
 auth2_client = fitbit.Fitbit(USER_ID, CLIENT_SECRET, oauth2=True, access_token=server.oauth.token['access_token'], refresh_token=server.oauth.token['refresh_token'])
 
-#for run in enumerate_activity_logs(auth2_client, 'mobile_run', '2016-12-01', '2017-01-01'):
-#    print(run['startTime'])
+# enumerate run logs.
+for run in enumerate_activity_logs(auth2_client, 'mobile_run', '2016-12-01', '2017-01-01'):
+    print(run['startTime'])
 
 # get TCX data and plot it.
 df = get_activity_tcx(auth2_client, LOG_ID)
